443b35c9d12e60c7c431b4db97c807f4d50f13ed
SouvikPaulCodes <169376487+SouvikPaulCodes@users.noreply.github.com>
2025-11-11 22:46:54 +0530

    Update redis.js

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -1,6 +1,7 @@
 const Redis = require('ioredis');
 
 const redis = new Redis({
+  username: 'default',
   host: process.env.REDIS_HOST || 'localhost',
   port: process.env.REDIS_PORT || 6379,
   password: process.env.REDIS_PASS,
